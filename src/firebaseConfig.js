7684a1aa910e18ecc882f95acd171d62baf4d15d
Carson Broz <cbroz@lion.lmu.edu>
2022-11-16 12:34:30 -0800

    Firebase Config Details Added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHIQjyq7vS8V2qnqEYU3jVJ-oPMfAIR1o",
+  authDomain: "project4-d0c90.firebaseapp.com",
+  projectId: "project4-d0c90",
+  storageBucket: "project4-d0c90.appspot.com",
+  messagingSenderId: "1097153308440",
+  appId: "1:1097153308440:web:20137d82d200960ab6606d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
