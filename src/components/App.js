997d45d702ff4c361646f5ab572fa287b596f77f
Kieran Ahn <kieranahn@gmail.com>
2022-11-28 11:48:26 -0800

    Added rudimentary search bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import ApodDisplay from './ApodDisplay'
 import { useState, useEffect } from 'react'
 import { KEY } from '../services/apiService'
+import Search from './Search'
 
 const apodURL = `https://api.nasa.gov/planetary/apod?api_key=${KEY}`
 
@@ -9,6 +10,7 @@ function App() {
   // const user = useAuthentication();
 
   const [apod, setApod] = useState(null)
+  const [data, setData] = useState(null)
 
   useEffect(() => {
     fetch(apodURL).then(response => {
@@ -23,6 +25,8 @@ function App() {
   return (
     <div className="App">
       <ApodDisplay apod={apod} />
+      <Search setData={setData} />
+      {data ? <img src={data.collection.items[0].links[0].href} /> : ''}
     </div>
   )
 }
