54ddd876d4edc2edb2d92d7303d62133cd024c28
Kieran Ahn <kieranahn@gmail.com>
2022-11-27 19:33:15 -0800

    Updated app to now display APOD

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css'
+import ApodDisplay from './ApodDisplay'
+import { useState, useEffect } from 'react'
+import { KEY } from '../services/apiService'
+
+const apodURL = `https://api.nasa.gov/planetary/apod?api_key=${KEY}`
 
 function App() {
   // const user = useAuthentication();
 
+  const [apod, setApod] = useState(null)
+
+  useEffect(() => {
+    fetch(apodURL).then(response => {
+      if (response.status >= 200 && response.status < 400) {
+        response.json().then(fulfilledRequest => setApod(fulfilledRequest))
+      } else {
+        setApod({ error: `Error ${response.status}` })
+      }
+    })
+  }, [apod])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ApodDisplay apod={apod} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
