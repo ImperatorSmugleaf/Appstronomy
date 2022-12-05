ed7c00ec122013e6170300c3c802066a94c13176
Kieran Ahn <kieranahn@gmail.com>
2022-12-05 13:32:44 -0800

    Added basic search results display component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
-const RESULTS_LENGTH = 100
-
 function Results({ data }) {
   const [resultsPerPage, setResultsPerPage] = useState(20)
   const [currentResults, setCurrentResults] = useState(0)
 
+  /*  TODO: Make a component called 'modal'; it has height and width a little less than the vh and vw. Put 
+            it into its own components. Only one prop: the image data. (Pick). Whenever you click on the 
+            thumbnail, open up the modal. Google how to do a modal. Make the background a transparent dark black.
+            The modal has an onClick or an x button to whenever you click on the black or the x to close the 
+            modal. */
+
   return data?.collection ? (
     data?.collection?.items.length > 0 ? (
-      [...data.collection.items.slice(currentResults, currentResults + resultsPerPage)]
+      data.collection.items
+        .slice(currentResults, currentResults + resultsPerPage)
+        .map(searchResult => <p>Not implemented yet!</p>)
     ) : (
       <p>No results found!</p>
     )
