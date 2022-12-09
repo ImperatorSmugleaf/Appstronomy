6a9ca90d910453056c254426ba97977bd5cacd7c
Kieran Ahn <kieranahn@gmail.com>
2022-12-08 16:23:02 -0800

    Added image details to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,34 @@
-function Modal({ source, setModal }) {
+function Modal({ pick, setPick }) {
   return (
-    <div id="modalBg" onClick={() => setModal(null)}>
+    <div id="modalBg" onClick={() => setPick(null)}>
       <main>
         <figure>
-          <figcaption>{source.data[0].title}</figcaption>
-          <img src={source.links[0].href} />
+          <figcaption>{pick.data[0].title}</figcaption>
+          <img src={pick.links[0].href} id="modalImg" />
         </figure>
+        <section>
+          <p>
+            <mark>Description:</mark> {pick.data[0].description}
+          </p>
+          <p>
+            <mark>Nasa ID:</mark> {pick.data[0].nasa_id}
+          </p>
+          {pick.data[0].photographer && (
+            <p>
+              <mark>Photographer:</mark> {pick.data[0].photographer}
+            </p>
+          )}
+          {pick.data[0].secondary_creator && (
+            <p>
+              <mark>Secondary Creator:</mark> {pick.data[0].secondary_creator}
+            </p>
+          )}
+          {pick.data[0].center && (
+            <p>
+              <mark>Published by:</mark> {pick.data[0].center}
+            </p>
+          )}
+        </section>
       </main>
     </div>
   )
