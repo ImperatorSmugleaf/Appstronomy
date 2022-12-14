bcb073d5b9de1e8f8099ca850ceb7f084ecd716f
Kieran Ahn <kieranahn@gmail.com>
2022-12-13 20:13:45 -0800

    Results now properly displays search results based on page number

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 
-function Results({ searchResults, setModal, currentResults, resultsPerPage }) {
+function Results({ searchResults, setModal, currentPageNumber, resultsPerPage }) {
   return searchResults ? (
     searchResults?.length > 0 ? (
-      searchResults.slice(currentResults, currentResults + resultsPerPage).map(searchResult => (
-        <figure onClick={() => setModal(searchResult)}>
-          <figcaption>{searchResult.data[0].title}</figcaption>
-          <img src={searchResult.links[0].href} className="thumbnail" />
-        </figure>
-      ))
+      searchResults
+        .slice(resultsPerPage * currentPageNumber, resultsPerPage * (currentPageNumber + 1))
+        .map(searchResult => (
+          <figure onClick={() => setModal(searchResult)}>
+            <figcaption>{searchResult.data[0].title}</figcaption>
+            <img src={searchResult.links[0].href} className="thumbnail" />
+          </figure>
+        ))
     ) : (
       <p className="text">No results found!</p>
     )
