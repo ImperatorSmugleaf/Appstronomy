a3d188fef61065bcd8c7b8c2087f3f1fa9f9231e
Kieran Ahn <kieranahn@gmail.com>
2022-12-14 05:30:43 -0800

    Updated addFavorite for changes in Firestore db. Firestore now stores the entire object for ease of use. Added fetchFavorites, which gets a user's favorites.

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -1,32 +1,30 @@
 import { db } from '../firebaseConfig'
-import { updateDoc, increment, arrayUnion, doc, getDoc, setDoc, getDocs } from 'firebase/firestore'
+import { updateDoc, increment, doc, getDoc, setDoc } from 'firebase/firestore'
 
-export async function addFavorite({ UserID, NasaID }) {
+export async function addFavorite({ UserID, media }) {
   const snapshot_user = await getDoc(doc(db, 'users', UserID))
-  const snapshot_pop = await getDoc(doc(db, 'popularMedia', NasaID))
   if (snapshot_user.exists()) {
-    var check = false
     const snap_fav = await getDoc(doc(db, 'users', UserID))
-    const arr = snap_fav.data().favorites
-    for (var i = 0; i < arr.length; i++) {
-      console.log(arr[i])
-      if (arr[i] == NasaID) {
-        check = true
-      }
-    }
-    if (check) {
+    const favorites = snap_fav.data().favorites
+
+    const NasaID = media.data[0].nasa_id
+
+    if (favorites[NasaID]) {
       console.log('Already in user favorites')
       return {}
     } else {
       console.log('Added to user favorites')
-      await updateDoc(doc(db, 'users', UserID), { favorites: arrayUnion(NasaID) })
+      favorites[NasaID] = media
+      await updateDoc(doc(db, 'users', UserID), { favorites: favorites })
+
+      const snapshot_pop = await getDoc(doc(db, 'popularMedia', NasaID))
       if (snapshot_pop.exists()) {
         console.log('Added to count')
         await updateDoc(doc(db, 'popularMedia', NasaID), { Count: increment(1) })
         return {}
       } else {
         console.log('Added to popular media')
-        await setDoc(doc(db, 'popularMedia', NasaID), { Count: 1 })
+        await setDoc(doc(db, 'popularMedia', NasaID), { Count: 1, media: media })
         return {}
       }
     }
@@ -34,4 +32,9 @@ export async function addFavorite({ UserID, NasaID }) {
     console.log('Need to be logged in to add photos to favorites.')
     return {}
   }
+}
+
+export async function fetchFavorites({ UserID }) {
+  const snapshot = await getDoc(doc(db, 'users', UserID))
+  return Object.values(snapshot.data()?.favorites)
 }
