b0dcd5010e3d0662ae2bbe00df62d3fbd3902cfd
Kieran Ahn <kieranahn@gmail.com>
2022-12-13 17:24:47 -0800

    Fixed close button position

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,10 @@ function Modal({ pick, setPick }) {
       <div id="modalBg" onClick={() => setPick(null)} />
       <main id="modalContent">
         <figure>
-          <figcaption>{pick.data[0].title}</figcaption> <button onClick={() => setPick(null)}>Close</button>
+          <figcaption>
+            {pick.data[0].title} <button onClick={() => setPick(null)}>Close</button>
+          </figcaption>
+
           <img src={pick.links[0].href} id="modalImg" />
         </figure>
         <section>
