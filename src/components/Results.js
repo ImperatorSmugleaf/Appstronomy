2e57732c4be8ebd7c99860822e96b3e022c4557d
Kieran Ahn <kieranahn@gmail.com>
2022-12-13 19:38:58 -0800

    Made text display better and reformatted for new variable types

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,19 +1,19 @@
 import { useState } from 'react'
 
 function Results({ searchResults, setModal, currentResults, resultsPerPage }) {
-  return searchResults?.collection ? (
-    searchResults?.collection?.items.length > 0 ? (
-      searchResults.collection.items.slice(currentResults, currentResults + resultsPerPage).map(searchResult => (
+  return searchResults ? (
+    searchResults?.length > 0 ? (
+      searchResults.slice(currentResults, currentResults + resultsPerPage).map(searchResult => (
         <figure onClick={() => setModal(searchResult)}>
           <figcaption>{searchResult.data[0].title}</figcaption>
           <img src={searchResult.links[0].href} className="thumbnail" />
         </figure>
       ))
     ) : (
-      <p>No results found!</p>
+      <p className="text">No results found!</p>
     )
   ) : (
-    <p>{searchResults}</p>
+    <p className="text">{searchResults}</p>
   )
 }
 
